291575ba6b81876865b7c606e7eb373a987b6a41
Inframan <ei12087@fe.up.pt>
2016-04-02 17:07:32 +0100

    restored order classes in payment

---
+++ Payment/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    class Order
+    {
+        public int Table { get; set; }
+        public int State { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+
+
+    }
+}
---
+++ Payment/OrderList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    class OrderList : MarshalByRefObject
+    {
+        private List<Order> list;
+
+        OrderList()
+        {
+            list = new List<Order>();
+        }
+
+        public void addOrder(Order o)
+        {
+            list.Add(o);
+        }
+
+        public List<Order> getList()
+        {
+            return list;
+        }
+
+
+        public List<Order> getTableOrders(int table)
+        {
+            List<Order> tables = new List<Order>();
+
+            foreach (Order o in list)
+            {
+                if (o.Table == table)
+                    tables.Add(o);
+            }
+
+            return tables;
+
+        }
+
+        public float getTablePrice(int table)
+        {
+            float total = 0f;
+
+            foreach (Order o in list)
+            {
+                total += o.Price;
+            }
+
+
+            return total;
+        }
+
+    }
+}
