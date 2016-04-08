3fdf42405d4ce95c599fdcfd2fd6ad25cb5557dc
Inframan <ei12087@fe.up.pt>
2016-04-08 22:35:18 +0100

    restructed tables and orders; renamed table to meal

--- Common/Common.cs
+++ Common/Common.cs
@@ -2,34 +2,89 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 [Serializable]
-public class Order {
-    public int Table { get; set; }
-    public int State { get; set; }
-    public int Id { get; set; }
-    public string Description { get; set; }
+public class Order
+{
     public int Quantity { get; set; }
-    public float Price { get; set; }
+    public float UnitPrice { get; set; }     
 
-    public Order(){    }
-
-    public Order(int table)
+    public float getTotalPrice()
     {
-        Table = table;
+        return (float) Quantity * UnitPrice;
+    }
+
+}
+
+
+[Serializable]
+public class Meal {
+    public Dictionary<string, Order> orders{ get; set; }
+    public int State { get; set; }
+    public int Table { get; set; }
+
+    public Meal(){ orders = new Dictionary<string, Order>();    }
+
+    public Meal(int num)
+    {
+        orders = new Dictionary<string, Order>();
+        Table = num;
+    }
+
+    public void addOrder(string name, Order o)
+    {
+        orders.Add(name, o);
+    }
+
+    public int incrementOrder(string name)
+    {
+        Order o;
+        if (orders.TryGetValue(name, out o))
+        {
+            o.Quantity = o.Quantity + 1;
+            orders.Remove(name);
+            orders.Add(name, o);
+            return o.Quantity;
+        }
+        else
+            throw new FieldAccessException();
+    }
+
+    public int Order(string name)
+    {
+        Order o;
+        if (orders.TryGetValue(name, out o))
+        {            
+            o.Quantity = o.Quantity - 1;
+            orders.Remove(name);
+            if (o.Quantity > 0)
+                orders.Add(name, o);
+            return o.Quantity;
+        }
+        else
+            throw new FieldAccessException();
+    }
+
+    public float totalPrice()
+    {
+        float price = 0f;
+        foreach (KeyValuePair<string, Order> o in orders)
+            price += o.Value.getTotalPrice();
+        return price;
     }
 
 }
 
 public enum Operation { New, Change };
 
-public delegate void AlterDelegate(Operation op, Order item);
+public delegate void AlterDelegate(Operation op, Meal item);
 
 public interface IListSingleton {
   event AlterDelegate alterEvent;
 
-  List<Order> GetList();
+  List<Meal> GetList();
   int GetNewType();
-  void AddItem(Order item);
+  void AddItem(Meal item);
   void ChangeComment(int type, string comment);
 }
 
@@ -40,7 +95,7 @@ public class AlterEventRepeater : MarshalByRefObject {
     return null;
   }
 
-  public void Repeater(Operation op, Order item) {
+  public void Repeater(Operation op, Meal item) {
     if (alterEvent != null)
       alterEvent(op, item);
   }
