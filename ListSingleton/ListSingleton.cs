85c7607e86109b0bb75af49f4d44d8c6dc952dc7
Inframan <ei12087@fe.up.pt>
2016-04-09 22:44:06 +0100

    missing file

--- ListSingleton/ListSingleton.cs
+++ ListSingleton/ListSingleton.cs
@@ -9,10 +9,13 @@ public class ListSingleton : MarshalByRefObject, IListSingleton
     public event AlterDelegate AlterEvent;
     public int Type = 2;
 
+    public Dictionary<string, Product> menu{  get; }
+
     public ListSingleton()
     {
         Console.WriteLine("Constructor called.");
         TablesList = new List<Table>();
+        initializeMenu();
     }
 
     public override object InitializeLifetimeService()
@@ -60,5 +63,28 @@ public class ListSingleton : MarshalByRefObject, IListSingleton
                 }).Start();
             }
         }
+    }
+
+
+    private void initializeMenu()
+    {
+        menu.Add("Água", new Product(0.7f, "Água"));
+        menu.Add("Coca-cola", new Product(1f, "Coca-cola"));
+        menu.Add("Super Bock", new Product(0.7f, "Super Bock"));
+        menu.Add("Vinho Bramco da Casa", new Product(0.7f, "Vinho Branco da Casa"));
+        menu.Add("Vinho Tinto da Casa", new Product(0.7f, "Vinho Tinto da Casa"));
+
+        menu.Add("Bacalhau com Natas", new Product(0.7f, "Bacalhau com Natas"));
+        menu.Add("Vitela Mendinha", new Product(0.7f, "Vitela Mendinha"));
+        menu.Add("Francesinha Especial", new Product(0.7f, "Francesinha Especial"));
+        menu.Add("Leitão no forno", new Product(0.7f, "Leitão no forno"));
+        menu.Add("Sandes de Leitão", new Product(0.7f, "Sandes de Leitão"));
+
+        menu.Add("Leite Creme", new Product(0.7f, "Leite Creme"));
+        menu.Add("Bolo do Bolacha", new Product(0.7f, "Bolo de Bolacha"));
+        menu.Add("Mousse de Manga", new Product(0.7f, "Mousse de Manga"));
+
+
+
     }
 }
